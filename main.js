36b5d7e9ca3165cd4759fe9258c9b571c4a03920
Jadan Morrow <jadanmorrow@outlookcom>
2020-05-26 09:42:55 -0600

    updating README.md

--- main.js
+++ main.js
@@ -1,74 +1,76 @@
-// START CODING BELOW!!
-// Initialize Firebase
-var config = {
-  apiKey: 'AIzaSyAFdyP4v-yKQXrtx-m-Wo8AassfVrf75MU',
-  authDomain: 'denverbootcamp-21006.firebaseapp.com',
-  databaseURL: 'https://denverbootcamp-21006.firebaseio.com',
-  projectId: 'denverbootcamp-21006',
-  storageBucket: 'denverbootcamp-21006.appspot.com',
-  messagingSenderId: '36508316501',
-  appId: '1:36508316501:web:f8b6688f025a9e59eeb5dc',
-  measurementId: 'G-HDVEGDNXZE'
-  // apiKey: "AIzaSyA5eYKsB8T2q6rMGdKSvac6eQsWTzsZEjE",
-  // authDomain: "fir-recent-user.firebaseapp.com",
-  // databaseURL: "https://fir-recent-user.firebaseio.com",
-  // storageBucket: "fir-recent-user.appspot.com"
-  // apiKey: "AIzaSyA3L6TkyFRcZdbMDCmcKSZB6vtUUe2xDuU",
-  // authDomain: "project-3701437879439973475.firebaseapp.com",
-  // databaseURL: "https://project-3701437879439973475.firebaseio.com",
-  // projectId: "project-3701437879439973475",
-  // storageBucket: "project-3701437879439973475.appspot.com",
-  // messagingSenderId: "919384740852",
-  // appId: "1:919384740852:web:376eead307f44e4550dfa1"
-};
-firebase.initializeApp(config);
-// Create a variable to reference the database.
-var database = firebase.database();
-// Initial Values
 var name = '';
-var email = '';
-var age = 0;
-var comment = '';
+var role = '';
+var date = 0;
+var salary = '';
+
 // Capture Button Click
 $('#add-user').on('click', function(event) {
   event.preventDefault();
-  // Grabbed values from text-boxes
-  newUserName = $('#name-input')
+
+  // Grabbed values from text boxes
+  name = $('#name-input')
     .val()
     .trim();
-  email = $('#email-input')
+
+  role = $('#role-input')
     .val()
     .trim();
-  startDate = $('#age-input')
+  date = $('#date-input')
     .val()
     .trim();
-  monthlyRate = $('#comment-input')
+  salary = $('#rate-input')
     .val()
     .trim();
-  // Code for "Setting values in the database"
-  // database.ref().push({
+
+  // Code for handling the push
   database.ref().push({
-    name: newUserName,
+    name: name,
     role: role,
-    age: startDate,
-    comment: monthlyRate
+    date: date,
+    salary: salary,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
+
+  $('#name-input').val('');
+  $('#role-input').val('');
+  $('#date-input').val('');
+  $('#rate-input').val('');
 });
-// Firebase watcher + initial loader HINT: .on("value")
+
+// Firebase watcher .on("child_added"
 database.ref().on(
   'child_added',
   function(snapshot) {
-    // Log everything that's coming out of snapshot
-    console.log(snapshot.val());
-    console.log(snapshot.val().name);
-    console.log(snapshot.val().email);
-    console.log(snapshot.val().age);
-    console.log(snapshot.val().comment);
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
+
+    // Console.loging the last user's data
+    console.log(sv.name);
+    console.log(sv.role);
+    console.log(sv.date);
+    console.log(sv.salary);
+
     // Change the HTML to reflect
-    $('#name-display').text(snapshot.val().name);
-    $('#email-display').text(snapshot.val().email);
-    $('#age-display').text(snapshot.val().age);
-    $('#comment-display').text(snapshot.val().comment);
+    // $("#name-display").text(sv.name);
+    // $("#role-input").text(sv.role);
+    // $("#date-inputy").text(sv.date);
+    // $("#rate-input").text(sv.salary);
+    var namedisplay = $('<td>');
+    var roledisplay = $('<td>');
+    var startdisplay = $('<td>');
+    var monthratedisplay = $('<td>');
+    var monthworkeddisplay = $('<td>');
+    var totalbilldisplay = $('<td>');
+
+    namedisplay.text(sv.name);
+    roledisplay.text(sv.role);
+    startdisplay.text(sv.date);
+    monthratedisplay.text(sv.salary);
+    monthworkeddisplay.text('');
+    totalbilldisplay.text('');
+
+    $('#empbody').append(namedisplay);
+
     // Handle the errors
   },
   function(errorObject) {
